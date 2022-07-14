b96366a9fc10b761ee7be3b0da0d4d1018759bf0
jerkoleto <66780009+kopiluwakbyte@users.noreply.github.com>
2022-07-14 09:19:21 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://website-starter-code.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
